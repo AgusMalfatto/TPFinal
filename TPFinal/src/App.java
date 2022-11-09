a7f9dc4e6bf5725504e3956d32b8afabb8f55e2e
AgusMalfatto <agus.malfatto@hotmail.com>
2022-11-09 01:37:44 -0300

    db update

---
+++ TPFinal/src/App.java
@@ -0,0 +1,9 @@
+import DataBase.Conection;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Conection conect = new Conection();
+        conect.addDBProd("Banana", "2025/05/11", 15, 15.3f, 10);
+        conect.showData();
+    }
+}
