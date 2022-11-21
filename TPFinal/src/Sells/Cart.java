7e4b33ba48cdf3228344de90b4c3ce9670929273
Gonzalo Valenzuela <gonzavalenzuela23@gmail.com>
2022-11-21 11:43:39 -0300

    Cart

--- TPFinal/src/Sells/Cart.java
+++ TPFinal/src/Sells/Cart.java
@@ -5,5 +5,13 @@ import java.util.ArrayList;
 public class Cart {
     private ArrayList<Product> products;
     
-    // public void 
+    public void addProduct(Product productito){
+        products.add(productito);
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void deleteProduct(){}
 }
